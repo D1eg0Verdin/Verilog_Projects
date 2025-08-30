33ab3243c3fc5c5e2e365cc5de9521f5f9fdae32
verdinrd <verdinrd@mcmaster.ca>
2025-08-30 14:12:06 -0400

    commented tb

--- seqLogic/upDownCounter/tb_counter.v
+++ seqLogic/upDownCounter/tb_counter.v
@@ -12,18 +12,20 @@ module tb_counter;
         .q(count)
     );
 
+    //clock
     initial begin
         clk = 0;
         forever #5 clk = ~clk ;
     end
 
     initial begin
+        //start with reset to ensure counter is 0
         rst = 1;
         up_down = 1;
         #20
         rst = 0;
         #100
-        up_down = 0;
+        up_down = 0; //swap counting directions
         #100
         $finish;
     end
@@ -32,6 +34,7 @@ module tb_counter;
         $display("Time = %0t |rst=%b| up_down = %b | count = %b (%0d)", $time, rst, up_down, count, count);
     end
 
+    //dump to vcd file
     initial begin
         $dumpfile("tb_counter.vcd");
         $dumpvars(0,tb_counter);
